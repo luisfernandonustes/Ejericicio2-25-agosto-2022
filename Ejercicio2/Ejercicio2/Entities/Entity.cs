c46b6e4b0de5bba6a3b57a29c8da7e594a83a458
luisfernandonustes <lfnustes@misena.edu.co>
2022-08-27 16:25:35 -0500

    polimorfismo

---
+++ Ejercicio2/Ejercicio2/Entities/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2.Entities
+{
+    internal class Entity
+    {
+        public string Id { get; protected set; }
+        public string Placa { get; protected set; }
+
+        public Entity(string id, string placa)
+        {
+            Id = id;
+            Placa = placa;
+        }
+    }
+}
--- Ejercicio2/Ejercicio2/Program.cs
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ejercicio2.Entities;
+//polimorfismo y caste (atributos)
+Entity ve = new VehiculoElectrico(id: "1", placa: "hga223", horasCarga: 50.1);
+var ve1 = (VehiculoElectrico)ve;
+Console.WriteLine(ve1.HorasCarga);
+
+//polimorfismo y caste (metodo)
+
+Entity vg = new VehiculoGasolina(id: "1", placa: "hga256");
+var vg1 = (VehiculoGasolina)vg;
+vg1.ImprimirValores();
+
+
